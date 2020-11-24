f4ecada8089b15e8d12bb7e24c33c45e60e0df60
FajarFvnky1 <74956670+FajarFvnky1@users.noreply.github.com>
2020-11-24 17:29:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const tambahan = require("./lib/tambahan.js")
 const donasi = require("./lib/donasi.js");
 const info = require("./lib/info.js");
 /////////////////
-const BotName = 'REUZ BOT☄'; 
-const instagram = 'https://instagram.com/reuz.id'; 
-const telegram = 'https://t.me/reuzzz'; 
+const BotName = 'FAJAR BOT☄'; 
+const instagram = 'https://www.instagram.com/fajarz01/'; 
+const telegram = 'GAK PUNYA'; 
 const kapanbotaktif = 'KETIKA HANYA DIAKTIFKAN'; 
-const youtube = 'https://www.youtube.com/c/reuz_id';
+const youtube = 'https://www.youtube.com/channel/UCE1voV6YJvAEr9hRHEvHc1g';
 //const grupch1 = 'belum ada grup'; 
 //const grupch2 = 'belum ada grup' ; 
 const
